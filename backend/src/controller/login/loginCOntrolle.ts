8f6a2eeca1f7e7c16724b4982d4e8761e8e869a9
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-12-24 18:54:00 -0300

    haaaaaa

---
+++ backend/src/controller/login/loginCOntrolle.ts
@@ -0,0 +1,10 @@
+import prismaClient from "../../prisma";
+import { Request, Response } from 'express';
+
+exports.listUsers = async (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
