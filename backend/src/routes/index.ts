1f4b6ab9d570b262406a79825de764720d522e0b
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-12-24 18:43:03 -0300

    zerado

--- backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { LoginUserController } from "../controllers/Login/LoginUserController";
-
-const router = Router()
-
-router.get("/me", )
-
-export { router };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,8 @@
 import express, { json } from 'express'
-import { router } from './routes';
 
 const app = express();
 
 app.use(json())
-app.use(router)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server on port ${port}`))
